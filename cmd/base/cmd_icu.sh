62a4d8e4e49411443ddfaaeb0e291c369e913465
midoks <midoks@163.com>
2019-04-14 12:42:28 +0800

    icu4c-intl

--- cmd/base/cmd_icu.sh
+++ cmd/base/cmd_icu.sh
@@ -12,20 +12,25 @@ mkdir -p $MDIR/source/cmd
 
 echo 'icu4c start'
 
-if [ ! -f $MDIR/source/cmd/icu4c-4_2_1-src.tgz ];then
-	wget -O $MDIR/source/cmd/icu4c-4_2_1-src.tgz http://download.icu-project.org/files/icu4c/4.2.1/icu4c-4_2_1-src.tgz
-fi
-
-if [ ! -d $MDIR/source/cmd/icu ];then
-	cd $MDIR/source/cmd &&  tar -zxvf icu4c-4_2_1-src.tgz
+if [ ! -d /usr/local/opt/icu4c ]; then
+	brew install icu4c
 fi
 
 
-if [ ! -d $DIR/cmd/icu ];then
+# if [ ! -f $MDIR/source/cmd/icu4c-4_2_1-src.tgz ];then
+# 	wget -O $MDIR/source/cmd/icu4c-4_2_1-src.tgz http://download.icu-project.org/files/icu4c/4.2.1/icu4c-4_2_1-src.tgz
+# fi
 
-cd $MDIR/source/cmd/icu/source
-./configure --prefix=$DIR/cmd/icu CXXFLAGS="-g -O2 -std=c++11" && make && make install
+# if [ ! -d $MDIR/source/cmd/icu ];then
+# 	cd $MDIR/source/cmd &&  tar -zxvf icu4c-4_2_1-src.tgz
+# fi
 
-fi
+
+# if [ ! -d $DIR/cmd/icu ];then
+
+# cd $MDIR/source/cmd/icu/source
+# ./configure --prefix=$DIR/cmd/icu CXXFLAGS="-g -O2 -std=c++11" && make && make install
+
+# fi
 echo 'icu4c end'
 
---
+++ php55/intl/load.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+VERSION=$1
+LIBNAME=intl
+
+echo "load $LIBNAME start"
+
+
+echo "" >> $DIR/php/php$VERSION/etc/php.ini
+echo "[${LIBNAME}]" >> $DIR/php/php$VERSION/etc/php.ini
+echo "extension=${LIBNAME}.so" >> $DIR/php/php$VERSION/etc/php.ini
+
+echo "load $LIBNAME end"
