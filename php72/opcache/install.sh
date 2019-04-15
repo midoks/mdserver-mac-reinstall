6a33aa8f66f22b7b359d3512cb535ab556346fe2
midoks <midoks@163.com>
2019-04-16 01:33:31 +0800

    opcache

---
+++ php72/opcache/install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+VERSION=$1
+LIBNAME=opcache
+LIBV='0'
+
+echo "install $LIBNAME start"
+echo "already install!"
+echo "install $LIBNAME end"
