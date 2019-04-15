47a8f8260b05d0eea2121555340e5c535b999223
Mr Chen <midoks@163.com>
2019-04-15 16:58:35 +0800

    gd

---
+++ php73/gd/install.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+VERSION=$1
+LIBNAME=gd
+LIBV='v'
+
+echo "install $LIBNAME start"
+
+extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20180731/${LIBNAME}.so
+
+isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${LIBNAME}.so'`
+if [ "${isInstall}" != "" ]; then
+	echo "php-$VERSION 已安装${LIBNAME},请选择其它版本!"
+	return
+fi
+
+if [ ! -f "$extFile" ]; then
+
+	cd $MDIR/source/php/php${VERSION}/ext/gd
+	$DIR/php/php$VERSION/bin/phpize
+	./configure --with-php-config=$DIR/php/php$VERSION/bin/php-config \
+	--with-zlib-dir=$DIR/cmd/zlib \
+	--with-png-dir=$DIR/cmd/libpng \
+	--with-freetype-dir=$DIR/cmd/freetype \
+	--with-jpeg-dir=$DIR/cmd/libjpeg && \
+	make && make install
+fi
+
+echo "install $LIBNAME end"
