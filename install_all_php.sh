2fb53f3f2cc127994f151a81eb9219487163fc19
Mr Chen <midoks@163.com>
2019-04-15 18:26:58 +0800

    prefect

--- install_all_php.sh
+++ install_all_php.sh
@@ -7,11 +7,142 @@ MDIR=$(dirname "$DIR")
 
 
 
+PHP_VER=53
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
+#cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
+echo "php${PHP_VER} -- end"
 
-# php53 start #
-cd $DIR/cmd/php53 && sh install.sh
-cd $DIR/cmd/php53/gd && sh install.sh
-# php53 start #
+
+
+PHP_VER=54
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
+#cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
+echo "php${PHP_VER} -- end"
+
+PHP_VER=55
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
+#cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=56
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
+#cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=70
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
+#cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=71
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
+#cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=72
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
+#cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
+echo "php${PHP_VER} -- end"
+
+
+
+PHP_VER=73
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
+#cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
+cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
+echo "php${PHP_VER} -- end"
+
 
 
 
--- php54/install.sh
+++ php54/install.sh
@@ -18,21 +18,20 @@ if [ ! -f $MDIR/source/php/php-${PHP_VER}.tar.bz2 ];then
 	wget -O $MDIR/source/php/php-${PHP_VER}.tar.bz2 https://museum.php.net/php5/php-${PHP_VER}.tar.bz2
 fi
 
-if [ ! -d $MDIR/source/php/php-${PHP_VER} ];then
-	cd $MDIR/source/php && tar -Jxf $MDIR/source/php/php-${PHP_VER}.tar.bz2
-fi
 
-echo $DIR
-if [ ! -f $MDIR/source/php/php${PHP_M_VER} ]; then
+
+if [ ! -d $MDIR/source/php/php${PHP_M_VER} ]; then
+	if [ ! -d $MDIR/source/php/php-${PHP_VER} ];then
+		cd $MDIR/source/php && tar -Jxf $MDIR/source/php/php-${PHP_VER}.tar.bz2
+	fi
+
 	mv $MDIR/source/php/php-${PHP_VER} $MDIR/source/php/php${PHP_M_VER}
 	cd $MDIR/source/php/php${PHP_M_VER}
 fi
 #./configure --help
 
-
 if [ ! -d $DIR/php/php${PHP_M_VER} ];then
 
-make clean
 
 ./configure \
 --prefix=$DIR/php/php${PHP_M_VER} \
