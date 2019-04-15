109e3ab4a28ed506fc823c73f417ace9e9472792
midoks <midoks@163.com>
2019-04-16 00:37:57 +0800

    intl

---
+++ php73/intl/install.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+VERSION=$1
+LIBNAME=intl
+LIBV=0
+
+echo "install $LIBNAME start"
+
+extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20180731/${LIBNAME}.so
+
+isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${LIBNAME}.so'`
+if [ "${isInstall}" != "" ]; then
+	echo "php-$VERSION 已安装${LIBNAME},请选择其它版本!"
+	return
+fi
+
+if [ ! -f "$extFile" ]; then
+
+	cd $MDIR/source/php/php${VERSION}/ext/intl
+	$DIR/php/php$VERSION/bin/phpize
+	echo `pwd`
+	./configure --with-php-config=$DIR/php/php$VERSION/bin/php-config \
+	--with-icu-dir=/usr/local/Cellar/icu4c/63.1  && \
+	make && make install
+fi
+
+echo "install $LIBNAME end"
