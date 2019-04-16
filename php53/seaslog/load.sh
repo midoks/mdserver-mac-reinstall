e1bfe8d9843acf921b9e50142f166f9290463cdb
midoks <midoks@163.com>
2019-04-16 21:52:35 +0800

    update

--- php53/seaslog/load.sh
+++ php53/seaslog/load.sh
@@ -21,5 +21,15 @@ fi
 echo "" >> $DIR/php/php$VERSION/etc/php.ini
 echo "[${LIBNAME}]" >> $DIR/php/php$VERSION/etc/php.ini
 echo "extension=${LIBNAME}.so" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.default_basepath=/Applications/mdserver/bin/tmp/logs" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.default_logger=default" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.disting_type=1" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.disting_by_hour=1" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.use_buffer=1" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.buffer_size=1" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.level=1" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.trace_error=1" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.trace_exception=1" >> $DIR/php/php$VERSION/etc/php.ini
+echo "${LIBNAME}.default_datetime_format=1" >> $DIR/php/php$VERSION/etc/php.ini
 
 echo "load $LIBNAME end"
