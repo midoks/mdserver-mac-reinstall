a598d2a82eb076af54676e14024af87a7b651aaa
midoks <midoks@163.com>
2019-07-02 21:05:53 +0800

    php54 check_common

--- php54/seaslog/install.sh
+++ php54/seaslog/install.sh
@@ -16,6 +16,8 @@ _LIBNAME=$(echo $LIBNAME | tr '[A-Z]' '[a-z]')
 
 echo "install $LIBNAME start"
 
+sh $MDIR/bin/reinstall/check_common.sh $VERSION
+
 extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20100525/${_LIBNAME}.so
 
 isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${_LIBNAME}.so'`
--- php54/swoole/install.sh
+++ php54/swoole/install.sh
@@ -18,6 +18,8 @@ fi
 
 echo "install $LIBNAME start"
 
+sh $MDIR/bin/reinstall/check_common.sh $VERSION
+
 extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20100525/${LIBNAME}.so
 
 isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${LIBNAME}.so'`
