5d78d9ef8f1d606e78bbf3d8ce18503530ca71df
midoks <midoks@163.com>
2019-07-02 20:43:09 +0800

    cmd - brew

---
+++ cmd/brew/install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+
+CHECK_BREW=`which brew`
+if [  "$CHECK_BREW" == "" ];then
+	echo "缺少brew命令,正在安装..."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
