10d38b6e5b8710d8cf609f2531f5787f2645d793
Mr Chen <midoks@163.com>
2019-07-02 17:29:57 +0800

    Update install.sh

--- php55/openssl/install.sh
+++ php55/openssl/install.sh
@@ -16,6 +16,29 @@ echo "install $LIBNAME start"
 
 extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20121212/${LIBNAME}.so
 
+if [ -f  $extFile ]; then
+	rm -rf $extFile
+fi
+
+
+CHECK_BREW=`which brew`
+if [  "$CHECK_BREW" == "" ];then
+	echo "缺少brew命令"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if [ ! -d /usr/local/Cellar/openssl ];then
+	brew install openssl
+fi
+
+LIB_DEPEND_DIR=`brew info openssl | grep /usr/local/Cellar/openssl | cut -d \  -f 1`
+
+
+echo "$LIBNAME-DIR:"
+echo $LIB_DEPEND_DIR
+
+
+
 isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${LIBNAME}.so'`
 if [ "${isInstall}" != "" ]; then
 	echo "php-$VERSION 已安装${LIBNAME},请选择其它版本!"
@@ -31,7 +54,7 @@ if [ ! -f "$extFile" ]; then
 
 	$DIR/php/php$VERSION/bin/phpize
 	./configure  --with-php-config=$DIR/php/php$VERSION/bin/php-config \
-	--with-openssl=$DIR/cmd/openssl && make && make install
+	--with-openssl=$LIB_DEPEND_DIR && make && make install
 fi
 
 echo "install $LIBNAME end"
