2b183dc6d1374e651532ecfe604a904084540370
midoks <midoks@163.com>
2019-07-03 21:08:13 +0800

    opt

--- php73/swoole/install.sh
+++ php73/swoole/install.sh
@@ -22,9 +22,6 @@ sh $MDIR/bin/reinstall/check_common.sh $VERSION
 
 extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20180731/${LIBNAME}.so
 
-if [ -f  $extFile ]; then
-	rm -rf $extFile
-fi
 
 
 isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${LIBNAME}.so'`
@@ -33,7 +30,15 @@ if [ "${isInstall}" != "" ]; then
 	return
 fi
 
+if [ -f  $extFile ]; then
+	rm -rf $extFile
+fi
+
+
 LIB_DEPEND_DIR=`brew info openssl | grep /usr/local/Cellar/openssl | cut -d \  -f 1`
+
+echo "$LIBNAME-DIR:"
+echo $LIB_DEPEND_DIR
 
 if [ ! -f "$extFile" ]; then
 
