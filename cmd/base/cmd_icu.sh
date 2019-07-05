7deb2bfaf9e7364607bd88a49dbfc213201c7191
Mr Chen <midoks@163.com>
2019-07-05 17:13:46 +0800

    Update cmd_icu.sh

--- cmd/base/cmd_icu.sh
+++ cmd/base/cmd_icu.sh
@@ -12,22 +12,21 @@ mkdir -p $MDIR/source/cmd
 
 echo 'icu4c start'
 
-
-
-if [ ! -f $MDIR/source/cmd/icu4c-4_8_2-src.tgz ];then
-	wget -O $MDIR/source/cmd/icu4c-4_8_2-src.tgz https://github.com/unicode-org/icu/releases/download/release-4-8-2/icu4c-4_8_2-src.tgz
-fi
-
-if [ ! -d $MDIR/source/cmd/icu ];then
-	cd $MDIR/source/cmd &&  tar -zxvf icu4c-4_8_2-src.tgz
-fi
-
-
 if [ ! -d $DIR/cmd/icu ];then
 
-cd $MDIR/source/cmd/icu/source
-./configure --prefix=$DIR/cmd/icu CXXFLAGS="-g -O2 -std=c++11" && make && make install
+	if [ ! -f $MDIR/source/cmd/icu4c-4_8_2-src.tgz ];then
+		wget -O $MDIR/source/cmd/icu4c-4_8_2-src.tgz https://github.com/unicode-org/icu/releases/download/release-4-8-2/icu4c-4_8_2-src.tgz
+	fi
+
+	if [ ! -d $MDIR/source/cmd/icu ];then
+		cd $MDIR/source/cmd &&  tar -zxvf icu4c-4_8_2-src.tgz
+
+		cd $MDIR/source/cmd/icu/source
+		./configure --prefix=$DIR/cmd/icu CXXFLAGS="-g -O2 -std=c++11" && make && make install
+	fi
 
 fi
+
+
 echo 'icu4c end'
 
