e246b2f476b6c1dbbccae085b8c3ecbda6d9b675
midoks <midoks@163.com>
2019-07-20 00:52:03 +0800

    up

--- cmd/mysql8/install.sh
+++ cmd/mysql8/install.sh
@@ -47,26 +47,33 @@ if [ ! -d $DIR/mysql8/data/mysql ];then
 fi
 
 
+#ijfk(<:xe7qP
+
 #mysql init pwd
 if [ ! -f $DIR/mysql8/data/mysql.log ]; then
 cd $DIR/mysql8/
 
 echo "$DIR/mysql8/bin/mysqld_safe \
---basedir=$DIR/mysql8 \
---datadir=$DIR/mysql8/data \
 --defaults-file=$DIR/tmp/my8.cnf \
 --user=mysql&"
-$DIR/mysql8/bin/mysqld_safe \
---basedir=$DIR/mysql8 \
---datadir=$DIR/mysql8/data \
+$DIR/mysql8/bin/mysqld \
 --defaults-file=$DIR/tmp/my8.cnf \
 --user=mysql&
 
-sleep 3
-$DIR/mysql/bin/mysqladmin --defaults-file=$DIR/tmp/my8.cnf -u root password root 
+PASSWORD=`cat $MDIR/bin/logs/reinstall/cmd_mysql8_install.log | grep root@localhost | awk -F 'root@localhost:' '{print $2}'` 
+PASSWORD=${PASSWORD%% }
+PASSWORD=${PASSWORD## }
+
+echo $PASSWORD
 
 sleep 3
-$DIR/mysql/bin/mysqladmin --defaults-file=$DIR/tmp/my8.cnf -uroot -proot shutdown
+
+# alter user user() identified by '123123';
+echo "$DIR/mysql8/bin/mysqladmin --defaults-file=$DIR/tmp/my8.cnf -u root password root"
+#$DIR/mysql8/bin/mysqladmin --defaults-file=$DIR/tmp/my8.cnf -u root password root 
+
+sleep 3
+#$DIR/mysql8/bin/mysqladmin --defaults-file=$DIR/tmp/my8.cnf -uroot -proot shutdown
 fi
 
 
---
+++ cmd/mysql8/start.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+cd $DIR/mysql8
+
+
+$DIR/mysql8/bin/mysqld \
+--defaults-file=$DIR/tmp/my8.cnf \
+--user=mysql&
