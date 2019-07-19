c00c1a9a2ffdcf66e7bf124632f8e110be9d90cb
Mr Chen <midoks@163.com>
2019-07-19 17:56:34 +0800

    mysql8

--- cmd/mysql8/install.sh
+++ cmd/mysql8/install.sh
@@ -15,48 +15,48 @@ mkdir -p $MDIR/source/mysql
 VERSION=8.0.16
 
 
-if [ ! -f $MDIR/source/mysql/mysql-${VERSION}-osx10.14-x86_64.tar.gz ]; then
+if [ ! -f $MDIR/source/mysql/mysql-${VERSION}-macos10.14-x86_64.tar.gz ]; then
 	wget -O $MDIR/source/mysql/mysql-${VERSION}-macos10.14-x86_64.tar.gz https://cdn.mysql.com//Downloads/MySQL-8.0/mysql-${VERSION}-macos10.14-x86_64.tar.gz
 fi
 
-if [ ! -d $MDIR/source/mysql/mysql-${VERSION}-macos10.14-x86_64.tar.gz ]; then
-	cd $MDIR/source/mysql8 && tar -zxvf $MDIR/source/mysql/mysql-${VERSION}-macos10.14-x86_64.tar.gz
+if [ ! -d $MDIR/source/mysql/mysql-${VERSION}-macos10.14-x86_64 ]; then
+	cd $MDIR/source/mysql && tar -zxvf $MDIR/source/mysql/mysql-${VERSION}-macos10.14-x86_64.tar.gz
 fi
 
 
-if [ ! -d $DIR/mysql ];then
-	mkdir -p $DIR/mysql
+if [ ! -d $DIR/mysql8 ];then
+	mkdir -p $DIR/mysql8
 	cp -r $MDIR/source/mysql/mysql-${VERSION}-macos10.14-x86_64/ $DIR/mysql8/
 fi
 
-if [ ! -d $DIR/tmp/my.cnf ]; then
+if [ ! -d $DIR/tmp/my8.cnf ]; then
 	mkdir -p $DIR/tmp
-	cp $MDIR/bin/reinstall/tpl/mysql/my.cnf $DIR/tmp/
+	cp $MDIR/bin/reinstall/tpl/mysql/my8.cnf $DIR/tmp/my8.cnf
 fi
 
 #mysql 5.1, 5.6版本,直接初始化表
-if [ ! -d $DIR/mysql/data/mysql ];then
-	$DIR/mysql/scripts/mysql_install_db \
-	--basedir=$DIR/mysql \
-	--datadir=$DIR/mysql/data \
-	--user=mysql
+if [ ! -d $DIR/mysql8/data/mysql ];then
+	$DIR/mysql8/bin/mysqld \
+	--basedir=$DIR/mysql8 \
+	--datadir=$DIR/mysql8/data \
+	--initialize
 
 	sleep 2
 fi
 
 
 #mysql init pwd
-if [ ! -f $DIR/mysql/data/mysql.log ]; then
-cd $DIR/mysql/
-$DIR/mysql/bin/mysqld_safe \
---defaults-file=$DIR/tmp/my.cnf \
+if [ ! -f $DIR/mysql8/data/mysql.log ]; then
+cd $DIR/mysql8/
+$DIR/mysql8/bin/mysqld_safe \
+--defaults-file=$DIR/tmp/my8.cnf \
 --user=mysql&
 
 sleep 3
-$DIR/mysql/bin/mysqladmin -u root password root
+$DIR/mysql/bin/mysqladmin --defaults-file=$DIR/tmp/my8.cnf -u root password root 
 
 sleep 3
-$DIR/mysql/bin/mysqladmin -uroot -proot shutdown
+$DIR/mysql/bin/mysqladmin --defaults-file=$DIR/tmp/my8.cnf -uroot -proot shutdown
 fi
 
 
