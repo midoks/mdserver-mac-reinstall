282413789e6882a816a604d227290ce3e89743ba
midoks <midoks@163.com>
2019-07-20 17:11:19 +0800

    Update install.sh

--- php72/mongodb/install.sh
+++ php72/mongodb/install.sh
@@ -19,12 +19,13 @@ TMP_CHECK_LOG=/tmp/t_check_php.log
 
 echo "extension=$LIBNAME.so" > $TMP_PHP_INI
 $DIR/php/php$VERSION/bin/php -c $TMP_PHP_INI -r 'phpinfo();' > $TMP_CHECK_LOG
-FIND_IS_INSTALL=`cat  $TMP_CHECK_LOG | grep "${LIBNAME}"`
+FIND_IS_INSTALL=`cat  $TMP_CHECK_LOG | grep "${LIBNAME}.debug"`
 
 echo "install $LIBNAME start"
 
 rm -rf $TMP_PHP_INI
 rm -rf $TMP_CHECK_LOG
+
 if [ "$FIND_IS_INSTALL" != "" ]; then
 	echo "install $LIBNAME end"	
 	exit 0
@@ -33,6 +34,10 @@ fi
 sh $MDIR/bin/reinstall/check_common.sh $VERSION
 
 extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20170718/${LIBNAME}.so
+
+if [ -f  $extFile ]; then
+	rm -rf $extFile
+fi
 
 isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${LIBNAME}.so'`
 if [ "${isInstall}" != "" ]; then
