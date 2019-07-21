d865340914645af6ca1a1c4d420fece04d73afa5
midoks <midoks@163.com>
2019-07-21 12:01:10 +0800

    up

--- cmd/mqtt/stop.sh
+++ cmd/mqtt/stop.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 
-
 DIR=$(cd "$(dirname "$0")"; pwd)
 DIR=$(dirname "$DIR")
 DIR=$(dirname "$DIR")
@@ -13,6 +12,5 @@ LOG_FILE=$MDIR/bin/logs/reinstall/cmd_mqtt_stop.log
 echo "stop!" > $LOG_FILE
 
 ps -ef | grep mosquitto | grep -v grep | awk -F ' ' '{print $2}' | xargs kill
-
 
 echo "ok!"
