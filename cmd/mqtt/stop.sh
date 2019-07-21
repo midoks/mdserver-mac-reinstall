5a1aed4d8fa5fd14586699d192300d80934b0792
midoks <midoks@163.com>
2019-07-21 11:10:55 +0800

    Update stop.sh

--- cmd/mqtt/stop.sh
+++ cmd/mqtt/stop.sh
@@ -9,7 +9,7 @@ DIR=$(dirname "$DIR")
 MDIR=$(dirname "$DIR")
 
 
-LOG_FILE=$MDIR/bin/logs/reinstall/cmd_mqtt_start.log
+LOG_FILE=$MDIR/bin/logs/reinstall/cmd_mqtt_stop.log
 echo "stop!" > $LOG_FILE
 
 ps -ef | grep mosquitto | grep -v grep | awk -F ' ' '{print $2}' | xargs kill
