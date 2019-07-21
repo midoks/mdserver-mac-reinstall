b1c42bc28c50c5b19897aec6ce82571ca6901e3c
midoks <midoks@163.com>
2019-07-21 18:00:27 +0800

    php

--- php55/mongodb/install.sh
+++ php55/mongodb/install.sh
@@ -35,6 +35,10 @@ sh $MDIR/bin/reinstall/check_common.sh $VERSION
 
 extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20121212/${LIBNAME}.so
 
+if [ -f  $extFile ]; then
+	rm -rf $extFile
+fi
+
 isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${LIBNAME}.debug'`
 if [ "${isInstall}" != "" ]; then
 	echo "php-$VERSION 已安装${LIBNAME},请选择其它版本!"
