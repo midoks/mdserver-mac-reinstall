04249349803a97f4560d4b3e68f62cf6120b7bf6
Mr Chen <midoks@163.com>
2019-07-24 19:09:19 +0800

    up

--- cmd/data-hadoop/install.sh
+++ cmd/data-hadoop/install.sh
@@ -2,6 +2,7 @@
 export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 
 DIR=$(cd "$(dirname "$0")"; pwd)
+PWD_DIR=$(cd "$(dirname "$0")"; pwd)
 DIR=$(dirname "$DIR")
 DIR=$(dirname "$DIR")
 DIR=$(dirname "$DIR")
@@ -23,15 +24,14 @@ if [ "$FIND_DOCKER" == "" ]; then
 fi
 
 mkdir -p $MDIR/source/hadoop
-if [ -f $MDIR/source/hadoop/hadoop-2.8.3.tar.gz ]; then
+if [ ! -f $MDIR/source/hadoop/hadoop-2.8.3.tar.gz ]; then
 	wget -O $MDIR/source/hadoop/hadoop-2.8.3.tar.gz http://archive.apache.org/dist/hadoop/common/hadoop-2.8.3/hadoop-2.8.3.tar.gz
 fi
 
-
+cp $MDIR/source/hadoop/hadoop-2.8.3.tar.gz $PWD_DIR/docker-hadoop/
+echo "docker build ./ -t $DOCKERNAME:$VERSION\r\n"
 cd  $MDIR/bin/reinstall/cmd/data-hadoop/docker-hadoop && docker build ./ -t $DOCKERNAME:$VERSION
-
-
-
+rm -rf $PWD_DIR/docker-hadoop/hadoop-2.8.3.tar.gz
 
 
 echo 'install hadoop end'
