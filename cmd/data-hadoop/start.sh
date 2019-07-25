76fb63e0ce19a2d53c118b642eaf0c725cf19c3c
Mr Chen <midoks@163.com>
2019-07-25 17:05:15 +0800

    update

--- cmd/data-hadoop/start.sh
+++ cmd/data-hadoop/start.sh
@@ -11,24 +11,67 @@ DOCKERNAME=hadoop
 VERSION=v1.0.0
 DOCKER_CON_NAME=hadoop_docker
 
-H_DAY=`date +%Y%m%d%H%M%S`
 
-DOCKER_CON_NAME_TIME=${DOCKER_CON_NAME}_${H_DAY}
+# ------------------  master start ---------------------
+H_DAY=`date +%Y%m%d%H%M%S`
+DOCKER_CON_NAME_TIME=${DOCKER_CON_NAME}_master_${H_DAY}
 
 
 LOG_FILE=$MDIR/bin/logs/reinstall/cmd_data-hadoop_start.log
 echo "start!" > $LOG_FILE
 
 echo $DOCKER_CON_NAME_TIME
-docker run  -p  9901:9001 -p 9970:50070 -p 9930:50030 \
+docker run  -p  9901:9001 \
+	-h master \
 	-d --cap-add=SYS_PTRACE \
 	--net=bridge \
 	--name $DOCKER_CON_NAME_TIME $DOCKERNAME:$VERSION
 
+SIGN=`docker ps | grep ${DOCKER_CON_NAME}_master | awk '{print $1}'`
 
-
-SIGN=`docker ps | grep $DOCKER_CON_NAME | awk '{print $1}'`
-
-echo "into shell:\r\n"
+echo "\r\n"
+echo "into master shell:"
 echo "docker exec -it $SIGN /bin/bash\r\n"
+# ------------------  master end -----------------------
+
+
+
+# ------------------  slave1 start ---------------------
+H_DAY=`date +%Y%m%d%H%M%S`
+DOCKER_CON_NAME_TIME=${DOCKER_CON_NAME}_slave1_${H_DAY}
+
+echo $DOCKER_CON_NAME_TIME
+docker run \
+	-h slave1 \
+	-d --cap-add=SYS_PTRACE \
+	--net=bridge \
+	--name $DOCKER_CON_NAME_TIME $DOCKERNAME:$VERSION
+
+SIGN=`docker ps | grep ${DOCKER_CON_NAME}_slave1 | awk '{print $1}'`
+
+echo "\r\n"
+echo "into slave2 shell:"
+echo "docker exec -it $SIGN /bin/bash\r\n"
+# ------------------  slave1 end -----------------------
+
+
+# ------------------  slave2 start ---------------------
+H_DAY=`date +%Y%m%d%H%M%S`
+DOCKER_CON_NAME_TIME=${DOCKER_CON_NAME}_slave2_${H_DAY}
+
+echo $DOCKER_CON_NAME_TIME
+docker run \
+	-h slave1 \
+	-d --cap-add=SYS_PTRACE \
+	--net=bridge \
+	--name $DOCKER_CON_NAME_TIME $DOCKERNAME:$VERSION
+
+SIGN=`docker ps | grep ${DOCKER_CON_NAME}_slave2 | awk '{print $1}'`
+
+echo "\r\n"
+echo "into slave1 shell:"
+echo "docker exec -it $SIGN /bin/bash\r\n"
+# ------------------  slave2 end -----------------------
+
+
 echo "ok!"
