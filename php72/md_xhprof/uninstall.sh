12b2745aa2512201d8fb3ec4eac198162ea1acc6
midoks <midoks@163.com>
2019-07-30 21:46:04 +0800

    md_xhprof脚本优化

--- php72/md_xhprof/uninstall.sh
+++ php72/md_xhprof/uninstall.sh
@@ -10,12 +10,3 @@ DIR=$(dirname "$DIR")
 MDIR=$(dirname "$DIR")
 
 echo $DIR
-
-
-#cd $MDIR/source/swoole-1.8.13/
-#echo $(pwd)
-
-#swoole install
-#$DIR/php/php55/bin/phpize
-#./configure --with-php-config=$DIR/php/php55/bin/php-config
-#make && make install && make clean
