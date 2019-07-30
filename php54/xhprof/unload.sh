42325531b29b304b13992c8b0d75c66fbd16a086
midoks <midoks@163.com>
2019-07-30 20:58:32 +0800

    php54 - add reload script

--- php54/xhprof/unload.sh
+++ php54/xhprof/unload.sh
@@ -18,4 +18,6 @@ sed -i '_bak' '/^$/N;/^\n$/D' $DIR/php/php$VERSION/etc/php.ini
 
 rm -rf $DIR/php/php$VERSION/etc/php.ini_bak
 
+$MDIR/bin/reinstall/reload.sh $VERSION
+
 echo "unload $LIBNAME end"
