53f2edcb79d588deda86eea0f0a652aee0cd12cc
Mr Chen <midoks@163.com>
2019-07-30 17:41:53 +0800

    Update load_env.sh

--- load_env.sh
+++ load_env.sh
@@ -21,5 +21,6 @@ echo 'load:'$USER
 echo "export PHP_VER=\"$PHP_M_VER\" #mdserver-mac loadenv" >> /Users/$USER/.bash_profile
 echo "alias php=\"/Applications/mdserver/bin/php/php\$PHP_VER/bin/php\" #mdserver-mac loadenv" >> /Users/$USER/.bash_profile
 echo "alias phpize=\"/Applications/mdserver/bin/php/php\$PHP_VAR/bin/phpize\" #mdserver-mac loadenv" >> /Users/$USER/.bash_profile
+echo "alias pecl=\"/Applications/mdserver/bin/php/php\$PHP_VAR/bin/pecl\" #mdserver-mac loadenv" >> /Users/$USER/.bash_profile
 
 source /Users/$USER/.bash_profile
