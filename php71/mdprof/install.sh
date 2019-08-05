682d1293da412d1118ced45262863848802ead5f
midoks <midoks@163.com>
2019-08-05 16:41:09 +0800

    Update install.sh

--- php71/mdprof/install.sh
+++ php71/mdprof/install.sh
@@ -41,10 +41,10 @@ if [ ! -f "$extFile" ]; then
 	fi
 
 	if [ -d $MDIR/source/php_lib/mdprof_run ];then
-		rm -rf $MDIR/source/php_lib/mdprof_run
+		rm -rf $MDIR/source/php_lib/mdprof_run/
 	fi
 
-	mkdir -p $MDIR/source/php_lib/mdprof_run
+	# mkdir -p $MDIR/source/php_lib/mdprof_run
 	cp -rf $MDIR/source/php_lib/mdprof/ $MDIR/source/php_lib/mdprof_run/
 
 	cd $MDIR/source/php_lib/mdprof_run
@@ -54,5 +54,7 @@ if [ ! -f "$extFile" ]; then
 	./configure --with-php-config=$DIR/php/php$VERSION/bin/php-config && \
 	make && make install && make clean
 fi
+
+$MDIR/bin/reinstall/reload.sh $VERSION
 
 echo "install $LIBNAME end"
