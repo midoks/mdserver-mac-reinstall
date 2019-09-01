ef8a3a9473adcf61fb0b11c3418c297d76308858
midoks <midoks@163.com>
2019-09-01 14:19:26 +0800

    docker go images

---
+++ cmd/docker-go/install.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+PWD_DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+
+DOCKERNAME=go
+VERSION=1.0.0
+DOCKER_CON_NAME=php71
+
+
+echo '' > $MDIR/bin/logs/reinstall/cmd_docker-go_install.log
+echo 'install go start'
+FIND_DOCKER=`which docker`
+
+if [ "$FIND_DOCKER" == "" ]; then
+	echo "please installed docker!"
+	exit 0
+fi
+
+mkdir -p $MDIR/source
+if [ ! -d $MDIR/source/docker-go ]; then
+	 cd $MDIR/source && git clone https://github.com/midoks/docker-go
+fi
+
+# if [ ! -d $MDIR/source/docker-php ]; then
+cd $MDIR/source/docker-go && git pull
+# fi
+
+
+
+echo "docker build -t $DOCKERNAME:$VERSION ./"
+docker build -t $DOCKERNAME:$VERSION ./
+
+
+echo 'install go end'
+
---
+++ cmd/docker-go/uninstall.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+DOCKERNAME=php71
+VERSION=1.0.0
+DOCKER_CON_NAME=php71
+
+LOG_FILE=$MDIR/bin/logs/reinstall/cmd_docker-go_uninstall.log
+echo "uninstall!" > $LOG_FILE
+
+
+echo 'ok'
