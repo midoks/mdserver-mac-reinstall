cee86fd7249374d5854cea704f82d4615bf0fb3f
midoks <midoks@163.com>
2019-10-26 01:37:23 +0800

    Update my_init.sh

--- cmd/mysql_debug/docker/my_init.sh
+++ cmd/mysql_debug/docker/my_init.sh
@@ -4,9 +4,9 @@ export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local
 
 /usr/local/mysql56/scripts/mysql_install_db --basedir=/usr/local/mysql56 --datadir=/usr/local/mysql56/data --user=mysql
 
-/usr/local/mysql56/bin/mysqld_safe &
+/usr/local/mysql56/bin/mysqld_safe --user=mysql &
 
-/usr/local/mysql56/bin/mysqladmin -u root password 'root'
+#/usr/local/mysql56/bin/mysqladmin -u root password 'root' shutdown
 
 echo "grant all PRIVILEGES on *.*  to root@'%'  identified by 'root'" > /tmp.sql
 /usr/local/mysql56/bin/mysql -uroot -proot < tmp.sql
