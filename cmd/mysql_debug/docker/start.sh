8f898fae4d7154079e25ac26958e4d5c937165ab
midoks <midoks@163.com>
2019-10-25 18:05:51 +0800

    Update start.sh

--- cmd/mysql_debug/docker/start.sh
+++ cmd/mysql_debug/docker/start.sh
@@ -1,15 +1,14 @@
 #! /bin/sh
 export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 
-echo "123123"
+echo "start"
 
 /usr/local/mysql56/bin/mysqld_safe &
 
-/usr/local/mysql56/bin/mysqladmin -u root password 'root'
-
-
 /usr/local/mysql56/scripts/mysql_install_db --basedir=/usr/local/mysql56 --datadir=/usr/local/mysql56/data --user=mysql &
 
+/usr/local/mysql56/bin/mysqladmin -u root password 'root'
 
 /usr/local/mysql56/bin/mysql -uroot -proot  < "grant all PRIVILEGES on *.*  to root@'%'  identified by '*';"
 
+echo "end"
