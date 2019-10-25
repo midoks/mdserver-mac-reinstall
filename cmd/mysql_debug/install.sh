bd5be051c475a20fe4bb2ccf2791841be3b6dc41
midoks <midoks@163.com>
2019-10-26 00:51:45 +0800

    Update install.sh

--- cmd/mysql_debug/install.sh
+++ cmd/mysql_debug/install.sh
@@ -30,9 +30,9 @@ fi
 cp -rf $MDIR/bin/reinstall/cmd/mysql_debug/docker/* $MDIR/source/mysql_debug/mysql-server
 
 
-if [ -d $MDIR/source/mysql_debug/mysql ]; then
-	rm -rf $MDIR/source/mysql_debug/mysql
-fi
+# if [ -d $MDIR/source/mysql_debug/mysql ]; then
+# 	rm -rf $MDIR/source/mysql_debug/mysql
+# fi
 
 cp -rf $MDIR/source/mysql_debug/mysql-server $MDIR/source/mysql_debug/mysql
 cd $MDIR/source/mysql_debug/mysql && rm -rf .git
