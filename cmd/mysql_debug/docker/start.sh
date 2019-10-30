cd4a38d30469340d880faf7c72318dcc4be857f7
midoks <midoks@163.com>
2019-10-31 00:14:43 +0800

    Update start.sh

--- cmd/mysql_debug/docker/start.sh
+++ cmd/mysql_debug/docker/start.sh
@@ -6,4 +6,22 @@ echo "start"
 export MYSQL_DEBUG=d:t:O,/tmp/client.trace
 
 
+if [ ! -f /init.lock ]; then
+	/usr/local/mysql56/scripts/mysql_install_db --basedir=/usr/local/mysql56 --datadir=/usr/local/mysql56/data --user=mysql
+	
+	sleep 3
+
+	/usr/local/mysql56/bin/mysqld_safe --debug --user=mysql &
+
+	sleep 3
+
+	/usr/local/mysql56/bin/mysqladmin -u root password 'root'
+
+	echo "grant all PRIVILEGES on *.*  to root@'%'  identified by 'root'" > /tmp/tmp.sql
+	/usr/local/mysql56/bin/mysql -uroot -proot < /tmp/tmp.sql
+	rm -f /tmp/tmp.sql
+
+	echo "" > /init.lock
+fi
+
 /usr/bin/supervisord -n -c /etc/supervisord.conf
