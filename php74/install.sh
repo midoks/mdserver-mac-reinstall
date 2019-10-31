761e7d5a4c5a729f6fdf3b31ee4499e4e49a001e
midoks <midoks@163.com>
2019-10-31 22:00:49 +0800

    Update install.sh

--- php74/install.sh
+++ php74/install.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+# https://wiki.php.net/todo/php74
+
 export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 
 
