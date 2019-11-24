2012058f6f49262c0568dc827275126e0d155b89
midoks <midoks@163.com>
2019-11-25 01:12:25 +0800

    xdebug fix

--- php55/xdebug/install.sh
+++ php55/xdebug/install.sh
@@ -10,17 +10,15 @@ MDIR=$(dirname "$DIR")
 
 VERSION=$1
 LIBNAME=xdebug
-LIBV=2.7.2
+LIBV=2.5.5
 
 #check
 TMP_PHP_INI=/tmp/t_tmp_php.ini
 TMP_CHECK_LOG=/tmp/t_check_php.log
 
-echo "extension=$LIBNAME.so" > $TMP_PHP_INI
+echo "zend_extension=$LIBNAME.so" > $TMP_PHP_INI
 $DIR/php/php$VERSION/bin/php -c $TMP_PHP_INI -r 'phpinfo();' > $TMP_CHECK_LOG
 FIND_IS_INSTALL=`cat  $TMP_CHECK_LOG | grep "${LIBNAME}"`
-echo $FIND_IS_INSTALL
-
 echo "install $LIBNAME start"
 
 rm -rf $TMP_PHP_INI
