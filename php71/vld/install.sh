0bf543f0b02ad382f0067a4bbcbbaf2b2576c909
midoks <midoks@163.com>
2019-11-25 00:55:05 +0800

    up

--- php71/vld/install.sh
+++ php71/vld/install.sh
@@ -12,13 +12,26 @@ VERSION=$1
 LIBNAME=vld
 LIBV=0.16.1
 
+#check
+TMP_PHP_INI=/tmp/t_tmp_php.ini
+TMP_CHECK_LOG=/tmp/t_check_php.log
+
+echo "extension=$LIBNAME.so" > $TMP_PHP_INI
+$DIR/php/php$VERSION/bin/php -c $TMP_PHP_INI -r 'phpinfo();' > $TMP_CHECK_LOG
+FIND_IS_INSTALL=`cat  $TMP_CHECK_LOG | grep "${LIBNAME}"`
+
 echo "install $LIBNAME start"
+
+rm -rf $TMP_PHP_INI
+rm -rf $TMP_CHECK_LOG
+if [ "$FIND_IS_INSTALL" != "" ]; then
+	echo "install $LIBNAME end"	
+	exit 0
+fi
 
 sh $MDIR/bin/reinstall/check_common.sh $VERSION
 
 extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20160303/${LIBNAME}.so
-
-
 if [ -f  $extFile ]; then
 	rm -rf $extFile
 fi
