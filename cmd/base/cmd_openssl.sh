291435551d155b1a4b0493c80af6d07d68252cb1
midoks <midoks@163.com>
2019-11-26 22:00:12 +0800

    Update cmd_openssl.sh

--- cmd/base/cmd_openssl.sh
+++ cmd/base/cmd_openssl.sh
@@ -12,20 +12,21 @@ mkdir -p $MDIR/source/cmd
 
 echo 'openssl start'
 
-if [ ! -f $MDIR/source/cmd/openssl-1.0.2n.tar.gz ];then
-	wget -O $MDIR/source/cmd/openssl-1.0.2n.tar.gz https://www.openssl.org/source/openssl-1.0.2n.tar.gz
+if [ ! -f $MDIR/source/cmd/openssl-1.0.1t.tar.gz ];then
+	wget -O $MDIR/source/cmd/openssl-1.0.1t.tar.gz https://www.openssl.org/source/old/1.0.1/openssl-1.0.1t.tar.gz
 fi
 
-if [ ! -d $MDIR/source/cmd/openssl-1.0.2n ];then
-	cd $MDIR/source/cmd &&  tar -zxvf openssl-1.0.2n.tar.gz
+if [ ! -d $MDIR/source/cmd/openssl-1.0.1t ];then
+	cd $MDIR/source/cmd &&  tar -zxvf openssl-1.0.1t.tar.gz
 fi
 
 
 if [ ! -d $DIR/cmd/openssl ];then
 
-cd $MDIR/source/cmd/openssl-1.0.2n
-./Configure darwin64-x86_64-cc --prefix=$DIR/cmd/openssl no-shared  && make && make install && make clean
+cd $MDIR/source/cmd/openssl-1.0.1t
 #sed -i "s/darwin-i386-cc/darwin64-x86_64-cc/g" Makefile
+./Configure darwin64-x86_64-cc --prefix=$DIR/cmd/openssl shared && make && make install && make clean
+sed -i "s/darwin-i386-cc/darwin64-x86_64-cc/g" Makefile
 fi
 echo 'openssl end'
 
