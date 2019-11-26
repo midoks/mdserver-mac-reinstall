f3d3c131a1672a5071d2d6be27450c6edf1c765a
midoks <midoks@163.com>
2019-11-26 14:52:21 +0800

    up

--- php74/gd/install.sh
+++ php74/gd/install.sh
@@ -13,7 +13,22 @@ LIBNAME=gd
 EXT_VERION=no-debug-non-zts-20190902
 LIBV='v'
 
+#check
+TMP_PHP_INI=/tmp/t_tmp_php.ini
+TMP_CHECK_LOG=/tmp/t_check_php.log
+
+echo "extension=$LIBNAME.so" > $TMP_PHP_INI
+$DIR/php/php$VERSION/bin/php -c $TMP_PHP_INI -r 'phpinfo();' > $TMP_CHECK_LOG
+FIND_IS_INSTALL=`cat  $TMP_CHECK_LOG | grep "${LIBNAME}"`
+
 echo "install $LIBNAME start"
+
+rm -rf $TMP_PHP_INI
+rm -rf $TMP_CHECK_LOG
+if [ "$FIND_IS_INSTALL" != "" ]; then
+	echo "install $LIBNAME end"	
+	exit 0
+fi
 
 sh $MDIR/bin/reinstall/check_common.sh $VERSION
 
