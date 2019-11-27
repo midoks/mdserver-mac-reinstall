a07093ca5843ac3faa0476bf09641e6e290ebd01
midoks <midoks@163.com>
2019-11-28 03:23:10 +0800

    Update install.sh

--- php71/install.sh
+++ php71/install.sh
@@ -30,7 +30,7 @@ fi
 
 #./configure --help
 if [ ! -d $DIR/php/php${PHP_M_VER} ];then
-
+cd $DIR/php/php${PHP_M_VER}
 ./configure \
 --prefix=$DIR/php/php${PHP_M_VER} \
 --exec-prefix=$DIR/php/php${PHP_M_VER} \
