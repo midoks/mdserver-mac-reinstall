74f36168f44d7f0fcc9d9297eb46055882d793fa
midoks <midoks@163.com>
2019-11-28 17:14:15 +0800

    opt

--- install_all_php.sh
+++ install_all_php.sh
@@ -7,237 +7,79 @@ MDIR=$(dirname "$DIR")
 
 
 
-# PHP_VER=53
-# echo "php${PHP_VER} -- start"
-# cd $DIR/php$PHP_VER && sh install.sh
-# cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-# #cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/msgpack && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/vld && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/gearman && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/seaslog && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/swoole && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/xhprof && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/solr && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/scws && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/opcache && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-# echo "php${PHP_VER} -- end"
+PHP_VER=53
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
+echo "php${PHP_VER} -- end"
 
 
-# PHP_VER=54
-# echo "php${PHP_VER} -- start"
-# cd $DIR/php$PHP_VER && sh install.sh
-# cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/msgpack && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/vld && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/gearman && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/seaslog && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/swoole && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/xhprof && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/solr && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/scws && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/opcache && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-# cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-# echo "php${PHP_VER} -- end"
+PHP_VER=54
+echo "php${PHP_VER} -- start"
+cd $DIR/php$PHP_VER && sh install.sh
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
+echo "php${PHP_VER} -- end"
 
 PHP_VER=55
 echo "php${PHP_VER} -- start"
 cd $DIR/php$PHP_VER && sh install.sh
-cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mongodb && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
-
-cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/msgpack && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/vld && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gearman && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/seaslog && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/swoole && sh install.sh $PHP_VER
-
-cd $DIR/php$PHP_VER/xhprof && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/solr && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/scws && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/xdebug && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/protobuf && sh install.sh $PHP_VER
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
 echo "php${PHP_VER} -- end"
 
 
 PHP_VER=56
 echo "php${PHP_VER} -- start"
 cd $DIR/php$PHP_VER && sh install.sh
-cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcache && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mongodb && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/msgpack && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/vld && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gearman && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/seaslog && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/swoole && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/xhprof && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/solr && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/scws && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/xdebug && sh install.sh $PHP_VER
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
 echo "php${PHP_VER} -- end"
 
 
 PHP_VER=70
 echo "php${PHP_VER} -- start"
 cd $DIR/php$PHP_VER && sh install.sh
-cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mongodb && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/msgpack && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/vld && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/seaslog && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/swoole && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/md_xhprof && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaconf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/solr && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/xdebug && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaml && sh install.sh $PHP_VER
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
 echo "php${PHP_VER} -- end"
 
 
 PHP_VER=71
 echo "php${PHP_VER} -- start"
 cd $DIR/php$PHP_VER && sh install.sh
-cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mongodb && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/msgpack && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/vld && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/seaslog && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/swoole && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/md_xhprof && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaconf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/solr && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/xdebug && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaml && sh install.sh $PHP_VER
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
 echo "php${PHP_VER} -- end"
 
 
 PHP_VER=72
 echo "php${PHP_VER} -- start"
 cd $DIR/php$PHP_VER && sh install.sh
-cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mongodb && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/msgpack && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/vld && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/seaslog && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/swoole && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/md_xhprof && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaconf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/xdebug && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaml && sh install.sh $PHP_VER
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
 echo "php${PHP_VER} -- end"
 
 
@@ -245,56 +87,21 @@ echo "php${PHP_VER} -- end"
 PHP_VER=73
 echo "php${PHP_VER} -- start"
 cd $DIR/php$PHP_VER && sh install.sh
-cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mongodb && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaf && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yar && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/msgpack && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/vld && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/seaslog && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/swoole && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/md_xhprof && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/xdebug && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaml && sh install.sh $PHP_VER
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
 echo "php${PHP_VER} -- end"
 
 
 PHP_VER=74
 echo "php${PHP_VER} -- start"
 cd $DIR/php$PHP_VER && sh install.sh
-cd $DIR/php$PHP_VER/gd && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/curl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/bcmath && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/iconv && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/imagick && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/redis && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/memcached && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/intl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mongodb && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mcrypt && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/gettext && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/openssl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/igbinary && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/rdkafka && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/mosquitto && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/pcntl && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/nsq && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/apcu && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/xdebug && sh install.sh $PHP_VER
-cd $DIR/php$PHP_VER/yaml && sh install.sh $PHP_VER
+dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+for i in $dir
+do
+	cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+done
 echo "php${PHP_VER} -- end"
 
