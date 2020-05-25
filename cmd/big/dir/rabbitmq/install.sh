81d92d07d314df33815fc92f41761496a16870e2
midoks <midoks@163.com>
2020-05-25 13:48:28 +0800

    up

---
+++ cmd/big/dir/rabbitmq/install.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+# VERSION=8.3.0
+APP_NAME=rabbitmq
+APP_DIR=$MDIR/source/${APP_NAME}
+mkdir -p $APP_DIR
+
+LOG_FILE=$MDIR/bin/logs/reinstall/cmd_big_dir_${APP_NAME}_install.log
+echo "-----------" > $LOG_FILE
+echo "install ${APP_NAME} start"
+
+brew install rabbitmq
+
+echo "install ${APP_NAME} end"
+echo "-----------" >> $LOG_FILE
---
+++ cmd/big/dir/rabbitmq/stop.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+# VERSION=8.3.0
+APP_NAME=rabbitmq
+APP_DIR=$MDIR/source/${APP_NAME}
+mkdir -p $APP_DIR
+
+LOG_FILE=$MDIR/bin/logs/reinstall/cmd_big_dir_${APP_NAME}_stop.log
+echo "-----------" > $LOG_FILE
+echo "stop ${APP_NAME} start"
+
+brew services stop rabbitmq
+
+
+echo "stop ${APP_NAME} end"
+echo "-----------" >> $LOG_FILE
