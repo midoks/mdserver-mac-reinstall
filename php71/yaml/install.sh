b451e4ad53acadd4ba036a0613c0b3c179521cdc
midoks <midoks@163.com>
2020-06-28 09:39:01 +0800

    yaml

--- php71/yaml/install.sh
+++ php71/yaml/install.sh
@@ -12,60 +12,6 @@ VERSION=$1
 LIBNAME=yaml
 LIBV=2.1.0
 
-
-#check
-TMP_PHP_INI=/tmp/t_tmp_php.ini
-TMP_CHECK_LOG=/tmp/t_check_php.log
-
-echo "extension=$LIBNAME.so" > $TMP_PHP_INI
-$DIR/php/php$VERSION/bin/php -c $TMP_PHP_INI -r 'phpinfo();' > $TMP_CHECK_LOG
-FIND_IS_INSTALL=`cat  $TMP_CHECK_LOG | grep "${LIBNAME}.decode_binary"`
-
-echo "install $LIBNAME start"
-
-rm -rf $TMP_PHP_INI
-rm -rf $TMP_CHECK_LOG
-if [ "$FIND_IS_INSTALL" != "" ]; then
-	echo "install $LIBNAME end"	
-	exit 0
-fi
-
-sh $MDIR/bin/reinstall/check_common.sh $VERSION
-
-extFile=$DIR/php/php$VERSION/lib/php/extensions/no-debug-non-zts-20160303/${LIBNAME}.so
-
-isInstall=`cat $DIR/php/php$VERSION/etc/php.ini|grep '${LIBNAME}.so'`
-if [ "${isInstall}" != "" ]; then
-	echo "php-$VERSION 已安装${LIBNAME},请选择其它版本!"
-	return
-fi
-
-if [ -f  $extFile ]; then
-	rm -rf $extFile
-fi
-
-
-if [ ! -f "$extFile" ]; then
-
-	php_lib=$MDIR/source/php_lib
-	mkdir -p $php_lib
-
-	if [ ! -f $php_lib/${LIBNAME}-${LIBV}.tgz ]; then
-		wget -O $php_lib/${LIBNAME}-${LIBV}.tgz http://pecl.php.net/get/${LIBNAME}-${LIBV}.tgz
-		
-	fi
-	cd $php_lib/${LIBNAME}-${LIBV}
-
-	if [ ! -d $php_lib/${LIBNAME}-${LIBV} ]; then
-		cd $php_lib
-		tar xvf ${LIBNAME}-${LIBV}.tgz
-	fi
-
-	cd $php_lib/${LIBNAME}-${LIBV}
-
-	$DIR/php/php$VERSION/bin/phpize
-	./configure --with-php-config=$DIR/php/php$VERSION/bin/php-config && \
-	make && make install && make clean
-fi
-
-echo "install $LIBNAME end"
+CONFIG_OPTION="|"
+FIND="yaml.decode_binary"
+sh $MDIR/bin/reinstall/ext_shell/install.sh $VERSION $LIBNAME $LIBV $CONFIG_OPTION $FIND
