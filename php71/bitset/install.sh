3324852dce4b875cf8a6006e4cd368c34bd4a0f1
midoks <midoks@163.com>
2021-08-22 00:33:28 +0800

    up

---
+++ php71/bitset/install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+DIR=$(dirname "$DIR")
+MDIR=$(dirname "$DIR")
+
+VERSION=$1
+LIBNAME=bitset
+LIBV=3.0.1
+
+CONFIG_OPTION="--enable-bitset"
+
+FIND="BitSet Version"
+sh $MDIR/bin/reinstall/ext_shell/install.sh $VERSION $LIBNAME $LIBV $CONFIG_OPTION $FIND
