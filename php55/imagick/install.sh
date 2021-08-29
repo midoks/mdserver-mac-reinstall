1f97096851eca76bd763bbd1fa493806ed6edc05
midoks <midoks@163.com>
2021-08-29 11:24:55 +0800

    up

--- php55/imagick/install.sh
+++ php55/imagick/install.sh
@@ -16,4 +16,5 @@ LIBV=3.4.4
 LIB_DEPEND_DIR=`brew info imagemagick | grep /usr/local/Cellar/imagemagick | cut -d \  -f 1 | awk 'END {print}'`
 CONFIG_OPTION="--with-imagick=${LIB_DEPEND_DIR}"
 
-sh $MDIR/bin/reinstall/ext_shell/install.sh $VERSION $LIBNAME $LIBV $CONFIG_OPTION
+FIND="imagick.locale_fix"
+sh $MDIR/bin/reinstall/ext_shell/install.sh $VERSION $LIBNAME $LIBV $CONFIG_OPTION $FIND
