47f1c164c3b78557074ab010d700f3c54abd757b
midoks <midoks@163.com>
2022-03-04 03:04:11 +0800

    up php version

--- php80/install.sh
+++ php80/install.sh
@@ -9,7 +9,7 @@ MDIR=$(dirname "$DIR")
 
 mkdir -p $MDIR/source/php
 
-PHP_VER=8.0.13
+PHP_VER=8.0.16
 PHP_M_VER=80
 
 if [ ! -f $MDIR/source/php/php-${PHP_VER}.tar.gz ];then
--- php81/install.sh
+++ php81/install.sh
@@ -9,7 +9,7 @@ MDIR=$(dirname "$DIR")
 
 mkdir -p $MDIR/source/php
 
-PHP_VER=8.1.0
+PHP_VER=8.1.3
 PHP_M_VER=81
 
 if [ ! -f $MDIR/source/php/php-${PHP_VER}.tar.gz ];then
