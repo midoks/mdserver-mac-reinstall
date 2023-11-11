967e2da12e5946232638c0923b548cdc70ecb5c6
Mr Chen <midoks@163.com>
2023-11-12 03:30:23 +0800

    update

--- php72/yaml/install.sh
+++ php72/yaml/install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
+export PATH=$PATH:/opt/homebrew/bin
 
 DIR=$(cd "$(dirname "$0")"; pwd)
 DIR=$(dirname "$DIR")
@@ -12,6 +13,10 @@ VERSION=$1
 LIBNAME=yaml
 LIBV=2.1.0
 
-CONFIG_OPTION="|"
+BREW_DIR=`which brew`
+BREW_DIR=${BREW_DIR/\/bin\/brew/}
+LIB_DEPEND_DIR=`brew info libyaml | grep ${BREW_DIR}/Cellar/libyaml | cut -d \  -f 1 | awk 'END {print}'`
+CONFIG_OPTION="--with-yaml=${LIB_DEPEND_DIR}"
+
 FIND="yaml.decode_binary"
 sh $MDIR/bin/reinstall/ext_shell/install.sh $VERSION $LIBNAME $LIBV $CONFIG_OPTION $FIND
