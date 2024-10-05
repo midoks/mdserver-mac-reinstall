0a11ca1f495f5dadd0651a0b821aaf803bdc893c
Mr Chen <midoks@163.com>
2024-10-06 01:13:12 +0800

    Update install_all_php.sh

--- install_all_php.sh
+++ install_all_php.sh
@@ -28,7 +28,8 @@ MDIR=$(dirname "$DIR")
 # cd /Applications/mdserver/bin/reinstall/php81 && bash install.sh
 
 
-PHP_VER_LIST=(55 56 71 72 73 74 80 81 82 83)
+PHP_VER_LIST=(55 56 71 72 73 74 80 81 82 83 84)
+# PHP_VER_LIST=(81)
 PHP_EXT_LIST=(curl openssl pcntl mcrypt fileinfo \
 	exif gd gettext zlib zip intl memcache memcached redis imagick xhprof swoole yaf mongodb iconv)
 
